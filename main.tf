432a6e8306b2378a33a787ab2a19098ec0c6e953
Sergii Kostiuk <qa.onpr@admiralmarkets.com>
2018-10-05 20:56:53 +0300

    fix ip_conf. added primary-tru

--- main.tf
+++ main.tf
@@ -78,9 +78,9 @@
 	#---------------------------Create Load Balancer-----------------------------
 	resource "azurerm_lb" "demo02group" {
 		name                = "${var.dns_name}-lb"
-			location            = "${var.location}"
-			resource_group_name = "${azurerm_resource_group.demo02group.name}"
-			tags = "${var.tags}"
+		location            = "${var.location}"
+		resource_group_name = "${azurerm_resource_group.demo02group.name}"
+		tags = "${var.tags}"
 
 		frontend_ip_configuration {
 			name                 = "${var.dns_name}-publicIPAddress"
@@ -125,42 +125,14 @@
 		probe_id                       = "${azurerm_lb_probe.demo02group.id}"
 	}
 
-	#-------------------Create Storage BLOB--------------------------------
-	# Create storage account for boot diagnostics
-	resource "azurerm_storage_account" "storageaccount" {
-		name    			              = "diag${random_string.fqdn.result}"
-		resource_group_name         = "${azurerm_resource_group.demo02group.name}"
-		location                    = "${var.location}"
-		account_tier                = "Standard"
-		account_replication_type    = "LRS"
-		tags                        = "${var.tags}"
-	}
-
-	resource "azurerm_storage_container" "demo02group" {
-		name = "demo02groupstor"
-		resource_group_name   = "${azurerm_resource_group.demo02group.name}"
-		storage_account_name  = "${azurerm_storage_account.storageaccount.name}"
-		container_access_type = "private"
-	}
-
-	resource "azurerm_storage_blob" "demo02group" {
-		name = "sample.vhd"
-		resource_group_name    = "${azurerm_resource_group.demo02group.name}"
-		storage_account_name   = "${azurerm_storage_account.storageaccount.name}"
-		storage_container_name = "${azurerm_storage_container.demo02group.name}"
-		type = "page"
-		size = "5120"
-	}
-
+	#-------------------Create Scale set--------------------------------
 	resource "azurerm_virtual_machine_scale_set" "demo02group" {
 		name                = "demo02group"
 		location            = "${var.location}"
 		resource_group_name = "${azurerm_resource_group.demo02group.name}"
 		upgrade_policy_mode = "Manual"
-		#tags    = "${var.tags}"
 
 		sku {
-			#name     = "Standard_B2s"
 			name     = "Standard_B1ms"
 			tier     = "Standard"
 			capacity = 2
@@ -199,20 +171,49 @@
 				path     = "/home/serg/.ssh/authorized_keys"
 				key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCpEDkv1gvNAvFire6/U979tlJNVIhZIerjGJM7c7XfQUZryVMcXm8tvlzYGEPTfUDYcSMWT5GCFLErCty+JjfX7cYooPX7UzQFFH1hkwMe+otrffeOx0nUyFR2Zrt7anc0tY2PAp1uFilyxVbtjGrHtteYPEyS/4oDQ2L1Buz5CP6DQESD2YKBhRaVPv9Q3KVOPev1AbLqmjM2Bp12RButqwW4tJIzuBD7reniIlY0hEBcWBWDf6NfdpnotClisUo7q1IyhXIs9gmEL95yyxe0ALoHAvBdyqtpEQ5RocVqqgOIwSQAeYKAazgpSWAUrFdh2Iw6fFT6jYHW1nVLXk8R serg@sergTravelMate"
 				#key_data = "${file("~/.ssh/id_rsa.pub")}"
-		}
-	}
-
-	network_profile {
-		name    = "${var.dns_name}-terraform-netw-profile"
-		primary = true
-		ip_configuration {
-			name                                   = "${var.dns_name}-IPConfiguration"
-			subnet_id                              = "${azurerm_subnet.demo02group.id}"
-			load_balancer_backend_address_pool_ids = ["${azurerm_lb_backend_address_pool.demo02group.id}"]
-			load_balancer_inbound_nat_rules_ids    = ["${element(azurerm_lb_nat_pool.demo02group.*.id, count.index)}"]
 			}
 		}
-		tags = "${var.tags}"
+
+		network_profile {
+			name    = "${var.dns_name}-terraform-netw-profile"
+			primary = true
+
+			ip_configuration {
+				primary 							   = true
+				name                                   = "${var.dns_name}-IPConfiguration"
+				subnet_id                              = "${azurerm_subnet.demo02group.id}"
+				load_balancer_backend_address_pool_ids = ["${azurerm_lb_backend_address_pool.demo02group.id}"]
+				load_balancer_inbound_nat_rules_ids    = ["${element(azurerm_lb_nat_pool.demo02group.*.id, count.index)}"]
+			}
+		}
+		tags    = "${var.tags}"
+	}
+
+	#-------------------Create Storage BLOB--------------------------------
+	# Create storage account for boot diagnostics
+	resource "azurerm_storage_account" "storageaccount" {
+		name    			        = "diag${random_string.fqdn.result}"
+		resource_group_name         = "${azurerm_resource_group.demo02group.name}"
+		location                    = "${var.location}"
+		account_tier                = "Standard"
+		account_replication_type    = "LRS"
+		tags                        = "${var.tags}"
+	}
+
+	resource "azurerm_storage_container" "demo02group" {
+		name 				  = "demo02groupstor"
+		resource_group_name   = "${azurerm_resource_group.demo02group.name}"
+		storage_account_name  = "${azurerm_storage_account.storageaccount.name}"
+		container_access_type = "private"
+	}
+
+	resource "azurerm_storage_blob" "demo02group" {
+		name = "sample.vhd"
+		resource_group_name    = "${azurerm_resource_group.demo02group.name}"
+		storage_account_name   = "${azurerm_storage_account.storageaccount.name}"
+		storage_container_name = "${azurerm_storage_container.demo02group.name}"
+		type = "page"
+		size = "5120"
 	}
 
 	# -------------- Create Mysql database ----------------------------
