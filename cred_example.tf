77b5f61191a5441f201f5b124ebba2d3a19fdeb3
Sergii Kostiuk <qa.onpr@admiralmarkets.com>
2018-10-05 17:22:13 +0300

    second init commit

---
+++ cred_example.tf
@@ -0,0 +1,15 @@
+	variable "subscription_id" {
+		default = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+	}
+
+	variable "client_id" {
+		default = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+	}
+
+	variable "client_secret" {
+		default = "xxxxxxxxxxxxxxxxx"
+	}
+
+	variable "tenant_id" {
+		default = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+	}
